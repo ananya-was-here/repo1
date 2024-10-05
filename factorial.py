20b0fde965bda09a7bbeec4cc547065ff36d257c
Ananya Gupta <ananya.was.here@gmail.com>
2024-10-05 19:03:30 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -5,3 +5,13 @@ def fact(n):
     return n*fact(n-1)
 inp=int(input("Enter any number"))
 print("it's factorial is",fact(inp))
+
+#or
+
+inp=int(input("Enter any number"))
+fact=1
+while inp>=1:
+  fact=fact*inp
+  inp=inp-1
+print(fact)
+  
